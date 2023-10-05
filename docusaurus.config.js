110f4cd25cba188ad4c877caf6fb2efd02039eb7
Dinh Bao Dang <250694+chillinPanda@users.noreply.github.com>
2023-10-05 16:21:13 +0200

    fix editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/chillinPanda/yoyobao.de/',
+            'https://github.com/chillinPanda/yoyobao.de/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/chillinPanda/yoyobao.de/',
+            'https://github.com/chillinPanda/yoyobao.de/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
